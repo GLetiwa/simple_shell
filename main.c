7d15f0bd3ea29b992f1276c233ca37b48f989901
GLetiwa <graceletiwa@gmail.com>
2023-08-09 19:32:50 +0300

    trial 1

---
+++ main.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+void tokenize_input(char *input);
+/**
+* int main -
+* @ac: 
+*@argv:
+*Return: 0 Always (Success)
+*/
+int main(int ac, char **argv)
+{
+	char *prompt = "(Eshell) $ ", *lineptr;
+	size_t n = 0; 
+	ssize_t chars_read;
+
+	(void)ac; (void)argv;
+	while (1)
+	{
+		printf("%s", prompt);
+		chars_read = getline(&lineptr, &n, stdin);
+		if (chars_read == -1)
+		{
+			printf("Exiting shell....\n");
+			free(lineptr);
+			return (-1);
+		}
+		printf("%s\n", lineptr);
+		tokenize_input(lineptr);
+		free(lineptr);
+		lineptr = NULL;
+	}
+	return (0);
+}
+void tokenize_input(char *input)
+{
+	char *delim = " \n", *token, *lineptr_copy;
+	int i, string_tokens = 0;
+	char **argx;
+	size_t chars_read = _strlen(input);
+	
+	lineptr_copy = malloc(sizeof(char) * chars_read);
+	if (!lineptr_copy)
+	{
+		perror("Memory allocation error");
+		return;
+	}
+	_strcpy(lineptr_copy, input);
+	token = strtok(input, delim);
+	while (1)
+	{
+		string_tokens++;
+		if (token)
+			token = strtok(NULL, delim);
+		else
+			break;
+	}
+	string_tokens++;
+	argx = malloc(sizeof(char *) * string_tokens);
+	token = strtok(lineptr_copy, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		argx[i] = malloc(sizeof(char) * _strlen(token));
+		_strcpy(argx[i], token);
+		token = strtok(NULL, delim);
+	}
+	argx[i] = NULL;
+	free(lineptr_copy);
+}
--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 
+/** FUNCTIONS **/
+int _strlen(const char *str);
+void _strcpy(char *dest, const char *src);
+int main(int ac, char **argv);
+void tokenize_input(char *input);
+void exec(char **argv);
 
 #endif
