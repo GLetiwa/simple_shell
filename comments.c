3f8fbd36193413153eeb54497be9481bb904c04e
GLetiwa <graceletiwa@gmail.com>
2023-08-22 22:40:17 +0300

    betty fixed

--- comments.c
+++ comments.c
@@ -19,7 +19,12 @@ void comment_handler(char *str_add)
 		}
 	}
 }
-
+/**
+ * check_logical_op - checks for a logical operator
+ * @argx: arguments passed
+ *
+ * Return: logical operator found
+ */
 int check_logical_op(char **argx)
 {
 	int i = 0;
@@ -38,48 +43,61 @@ int check_logical_op(char **argx)
 	}
 	return (NO_LOGICAL_OP);
 }
-char **get_left_command(char **argx, int logical_op) 
+/**
+ * get_left_command - gets the command at the left side of log op
+ * @argx: arguments passed
+ * @logical_op: logical operator in place
+ *
+ * Return: command at the left side of the logical op
+ */
+char **get_left_command(char **argx, int logical_op)
 {
 	int i = 0, count = 0;
 	char **left_cmd = NULL;
 
-	while (argx[i] && i < logical_op) 
+	while (argx[i] && i < logical_op)
 	{
 		count++;
 		i++;
 	}
 	left_cmd = malloc((count + 1) * sizeof(char *));
-	if (!left_cmd) 
+	if (!left_cmd)
 	{
 		perror("Memory allocation error");
 		return (NULL);
 	}
-	for (i = 0; i < count; i++) 
+	for (i = 0; i < count; i++)
 	{
 		left_cmd[i] = strdup(argx[i]);
 	}
 	left_cmd[count] = NULL;
 	return (left_cmd);
 }
-
-char **get_right_command(char **argx, int logical_op) 
+/**
+ * get_right_command -gets the command at the right side of log op
+ * @argx: arguments passed
+ * @logical_op: logical operator in place
+ *
+ * Return: command at the right side of the logical operator
+ */
+char **get_right_command(char **argx, int logical_op)
 {
 	int i = logical_op + 1, count = 0;
 	char **right_cmd = NULL;
 
-	while (argx[i] && argx[i + count]) 
+	while (argx[i] && argx[i + count])
 	{
 		count++;
 	}
 	right_cmd = malloc((count + 1) * sizeof(char *));
-	if (!right_cmd) 
+	if (!right_cmd)
 	{
 		perror("Memory allocation error");
 		return (NULL);
 	}
-	for (i = logical_op + 1; i < logical_op + count; i++) 
+	for (i = logical_op + 1; i < logical_op + count; i++)
 	{
-		right_cmd[i - logical_op - 1] = strdup(argx[i]);
+		right_cmd[i - (logical_op + 1)] = strdup(argx[i]);
 	}
 	right_cmd[count] = NULL;
 	return (right_cmd);
